38f97dd646519edd9ec11480cd0563a56d183bad
Ebrahim12345 <Ebrahim1989@yahoo.com>
2023-10-14 20:31:51 +0330

    Initial commit

---
+++ src/main/java/com/housekeeping/cleaningservicecompany/CleaningServiceCompanyApplication.java
@@ -0,0 +1,13 @@
+package com.housekeeping.cleaningservicecompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleaningServiceCompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleaningServiceCompanyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/housekeeping/cleaningservicecompany/CleaningServiceCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.housekeeping.cleaningservicecompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleaningServiceCompanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
